030e48c6941193382325df7d1c1ae9835eea26fc
Anamuddin Ahmad <119073478+AnamuddinAhmad@users.noreply.github.com>
2024-03-14 10:11:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+let newelem=document.createElement("p");
+
+let rangenChoice = () => {
+  let option = ["rock", "paper", "sicissor"];
+  let index = Math.floor(Math.random() * 3);
+  return option[index];
+};
+
+let playGame = (userChoic) => {
+  let compchoice = rangenChoice();
+  if (compchoice === userChoic) {
+    console.log("math draw");
+    console.log("user choice ",userChoic);
+    console.log("comp choice ",compchoice);
+    let result =document.querySelector("#resutl-board");
+    newelem.style.display="none";
+    result.innerHTML=`${"Match Draw"}`;
+    result.style.backgroundColor="gray";
+  }
+  else if (
+    (userChoic === "rock" && compchoice === "paper") ||
+    (userChoic === "paper" && compchoice === "sicissor") ||
+    (compchoice==="rock" && userChoic==="sicissor")
+  ) {
+    compScore++;
+    newelem.style.display="block";
+    document.querySelector("#comp-score").innerHTML=compScore;
+    let result =document.querySelector("#resutl-board");
+    console.log("user choice ",userChoic);
+    console.log("comp choice ",compchoice);
+    console.log("comp ",compScore);
+    result.innerHTML=`${"You Lost!"}`;
+    newelem.style.backgroundColor="green";
+    newelem.classList.add("newelem");
+    newelem.innerText=`You Choose ${userChoic} and Computer Choose ${compchoice}`
+    result.after(newelem);
+    newelem.style.backgroundColor="red";
+    result.style.backgroundColor="red";
+  }else{
+    userScore++;
+    newelem.style.display="block";
+    document.querySelector("#user-score").innerHTML=compScore;
+    let result =document.querySelector("#resutl-board");
+    console.log("user choice ",userChoic);
+    console.log("comp choice ",compchoice);
+    result.innerHTML=`You Win!`;
+    newelem.style.backgroundColor="green";
+    newelem.classList.add("newelem");
+    newelem.innerText=`You Choose ${userChoic} and Computer Choose ${compchoice}`
+    result.after(newelem);
+    result.style.backgroundColor="green";
+    console.log("user ",userScore);
+  }
+  //genrating the radom vlue;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    let userChoic = choice.getAttribute("id");
+    playGame(userChoic);
+  });
+});
